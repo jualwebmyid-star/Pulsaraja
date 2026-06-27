dfa736f995f5fe1d8e6d4ab517edbb1c6826a735
jualwebmyid-star <jualwebmyid@gmail.com>
2026-06-28 00:07:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,19 @@
+const CACHE_NAME = 'pulsaraja-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (e) => {
-  // Ambil dari internet dulu. Gagal? Baru ambil cache.
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
